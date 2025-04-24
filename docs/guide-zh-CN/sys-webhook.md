e7bff32f06fa47024e5416a59a113bfc7d750d5d
zty188 <53037371@qq.com>
2025-04-24 17:13:35 +0800

    发布v2.2.10版本，更新内容请查看：https://github.com/zty188/qyhaoka/tree/main/docs/guide-zh-CN/addon-version-upgrade.md

--- docs/guide-zh-CN/sys-webhook.md
+++
@@ -1,3 +0,0 @@
-## WebHook
-
-请参考：https://goframe.org/pages/viewpage.action?pageId=1114387
--- server/addons/hgexample/logic/logic.go
+++
@@ -1,9 +0,0 @@
-// ==========================================================================
-// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
-// ==========================================================================
-
-package logic
-
-import (
-	_ "hotgo/addons/hgexample/logic/sys"
-)
